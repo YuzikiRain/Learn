503cc7b91695869873bd9c76133dfc181ef5d981
YukiRain <1404094167@qq.com>
2019-02-15 11:29:31 +0800

    ReverseWordsInAString_151

--- Code/LeetCode/LeetCode_Qinyu/Program.cs
+++ Code/LeetCode/LeetCode_Qinyu/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var test = new LeetCode_Qinyu.ReverseWordsInAString_557.ReverseWords_557();
+        var test = new LeetCode_Qinyu.ReverseWordsInAString_151.ReverseWordsInAString_151();
         test.Test();
 
         Console.ReadLine();
