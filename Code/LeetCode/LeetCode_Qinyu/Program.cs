2bb2bc7a14f13b08a6c4de69abe16c806f7091a4
YukiRain <1404094167@qq.com>
2019-02-26 19:39:08 +0800

    [add] 19. Remove Nth Node From End of List

--- Code/LeetCode/LeetCode_Qinyu/Program.cs
+++ Code/LeetCode/LeetCode_Qinyu/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var test = new LeetCode_Qinyu.ReverseWordsInAString_151.ReverseWordsInAString_151();
+        var test = new LeetCode_Qinyu.RemoveNthFromEnd_19.RemoveNthFromEnd_19();
         test.Test();
 
         Console.ReadLine();
