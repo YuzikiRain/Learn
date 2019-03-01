a479eb2a90c69a4850de4cb32cae4d98d5224ccd
YukiRain <1404094167@qq.com>
2019-03-01 12:57:09 +0800

    [add] leetcode: BinaryTreePreorderTraversal_144

--- Code/LeetCode/LeetCode_Qinyu/MaximumDepthOfBinaryTree_104/MaximumDepthOfBinaryTree_104.cs
+++ Code/LeetCode/LeetCode_Qinyu/MaximumDepthOfBinaryTree_104/MaximumDepthOfBinaryTree_104.cs
@@ -1,14 +1,18 @@
-﻿namespace LeetCode_Qinyu.MaximumDepthOfBinaryTree_104
+﻿using System;
+
+namespace LeetCode_Qinyu.MaximumDepthOfBinaryTree_104
 {
     class MaximumDepthOfBinaryTree_104
     {
         public void Test()
         {
+            //TreeNode root = null;
             TreeNode root = new TreeNode(3);
             root.left = new TreeNode(9);
             root.right = new TreeNode(20);
             root.right.left = new TreeNode(15);
             root.right.right = new TreeNode(7);
+            root.right.left.right = new TreeNode(33);
             var result = MaxDepth(root);
             System.Console.WriteLine($"MaximumDepthOfBinaryTree is {result}");
         }
@@ -22,10 +26,8 @@
             }
             else
             {
-                int left = MaxDepth(root.left);
-                int right = MaxDepth(root.right);
                 // this node exists, so depth add one
-                return left > right ? left + 1 : right + 1;
+                return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
             }
         }
 
--- Code/LeetCode/LeetCode_Qinyu/Program.cs
+++ Code/LeetCode/LeetCode_Qinyu/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var test = new LeetCode_Qinyu.BinaryTreeLevelOrderTraversal_102.BinaryTreeLevelOrderTraversal_102();
+        var test = new LeetCode_Qinyu.BinaryTreePreorderTraversal_144.BinaryTreePreorderTraversal_144();
         test.Test();
 
         Console.ReadLine();
