765b9573b82a497762bd57047d7860d7f37ced54
覃雨 <1404094167@qq.com>
2020-02-29 00:03:34 +0800

    [add] 剑指offer面试题19 正则表达式匹配

---
+++ Code/LeetCode/LeetCode_Yuki/BinaryTreePreorderTraversal_144/BinaryTreePreorderTraversal_144.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_Yuki.BinaryTreePreorderTraversal_144
+{
+    class BinaryTreePreorderTraversal_144
+    {
+        public void Test()
+        {
+            //TreeNode root = null;
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            IList<int> list = PreorderTraversal(root);
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        #region by iteration
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            while (true)
+            {
+                while (root != null)
+                {
+                    // visit root
+                    result.Add(root.val);
+                    // cache root's right subtree
+                    if (root.right != null)
+                        stack.Push(root.right);
+                    // visit along with left subtree
+                    root = root.left;
+                }
+
+                // no left subtree, so pop cached right subtree to continue
+                if (stack.Count > 0)
+                {
+                    root = stack.Pop();
+                }
+                // no cached right subtree, end
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region by recursion
+
+        //public IList<int> PreorderTraversal(TreeNode root)
+        //{
+        //    IList<int> result = new List<int>();
+        //    PreorderTraversalByRecursion(root, result);
+        //    return result;
+        //}
+
+        //public void PreorderTraversalByRecursion(TreeNode root, IList<int> result)
+        //{
+        //    if (root != null)
+        //    {
+        //        result.Add(root.val);
+        //        PreorderTraversalByRecursion(root.left, result);
+        //        PreorderTraversalByRecursion(root.right, result);
+        //    }
+        //}
+
+        #endregion
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
---
+++ Code/LeetCode/LeetCode_Yuki/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var test = new Offer_RegularExpression_19();
+        test.Test();
+
+        Console.ReadLine();
+    }
+
+}
+
