21ad3c664ad81132a90e633256c272cdd493bdfd
覃雨 <1404094167@qq.com>
2020-02-29 00:03:34 +0800

    [add] 剑指offer面试题19 正则表达式匹配

---
+++ Code/LeetCode/LeetCode_Yuki/Fizz Buzz_412/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_Yuki.Fizz_Buzz_412
+{
+    class FizzBuzz_412
+    {
+        public void Test()
+        {
+            var list = FizzBuzz(100);
+            foreach(var number in list)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public IList<string> FizzBuzz(int n)
+        {
+            string[] result = new string[n];
+            int three = 3;
+            int five = 5;
+            int fifteen = 15;
+            for(int i = 0; i < n; i++)
+            {
+                if (i + 1 == fifteen)
+                {
+                    result[i] = "FizzBuzz";
+                    fifteen += 15;
+                    three += 3;
+                    five += 5;
+                }
+                else if (i + 1 == three)
+                {
+                    result[i] = "Fizz";
+                    three += 3;
+                }
+                else if (i + 1 == five)
+                {
+                    result[i] = "Buzz";
+                    five += 5;
+                }
+                else
+                {
+                    result[i] = (i + 1).ToString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Code/LeetCode/LeetCode_Yuki/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var test = new RegularExpression_19();
+        test.Test();
+
+        Console.ReadLine();
+    }
+
+}
+
